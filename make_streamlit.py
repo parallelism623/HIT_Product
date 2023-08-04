b6febcecaa714f4e2752a3f6f793ba9d03d2227a
Phạm Đăng Đông <110766567+Anodis108@users.noreply.github.com>
2023-08-04 14:01:48 +0700

    Update make_streamlit.py

--- make_streamlit.py
+++ make_streamlit.py
@@ -6,7 +6,6 @@ import tempfile
 # Khách hàng vào trang, sẽ có ô để tải ảnh lên, 1 ô để chọn style, 1 ô để show kết quả
 # 1 nút để tải hình ảnh về
 # Thêm file
-#Chạy chương trình: streamlit run make_streamlit.py
 
 # # Bước 1: Tạo ra fodel chứa tất cả các hình ảnh chạy thử nghiệm với path
 # model_load_path = 'result/'
@@ -36,6 +35,7 @@ with col1:
 #st.text(type(file))
 #Col 2: Show ra các style ảnh để khách hàng chọn style Chọn file style
 selected_image = ''
+image_path = ''
 with col2:
     st.title("Chọn ảnh yêu thích")
 
@@ -48,13 +48,7 @@ with col2:
     if selected_image:
         image_path = os.path.join(image_folder, selected_image)
         st.image(image_path, caption=selected_image, use_column_width=True)
-        print(selected_image[selected_image.rfind('/'):-4])
-        model_load_paths = 'result'
         
-        for i in os.listdir(model_load_paths):
-            if selected_image[selected_image.rfind('/'):-4] == i[i.rfind('/'):-4]:
-                selected_image = i
-                break
 check = False
 result = []
 #Thực hiện generate và show ra 
@@ -62,7 +56,10 @@ with col3:
 
     st.title("Chạy Mô Hình với Streamlit")
 
-    model_load_path = selected_image
+    model_load_paths = 'result/'
+    st.write(image_path)
+    image_path = image_path[(image_path.rfind('\\') + 1):-4]
+    model_load_path = os.path.join(model_load_paths, f'{image_path}.pth') # file .pth
 
     desired_temp_dir = "D:/A_HIT_AI/Deep_Learning/HIT_Product/test_result"
     temp_dir = os.path.join(desired_temp_dir)
@@ -71,13 +68,13 @@ with col3:
     temp_image_path = os.path.join(temp_dir, "temp_image.png")
     with open(temp_image_path, 'wb') as temp_file:
         temp_file.write(file.read())
-    test_content_path = temp_image_path 
+    test_content_path = temp_image_path # file content
         
     output_path = "test_result"
 
     if st.button("Chạy"):
         check = True
-        command = f"python test_main.py --model_load_path result/{model_load_path} --test_content {test_content_path} --output {output_path}/tt.png"
+        command = f"python test_main.py --model_load_path {model_load_path} --test_content {test_content_path} --output {output_path}/tt.png"
         process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
         output, error = process.communicate()
         result = f'{output_path}/tt.png'
