43e9318889c3b7c7360b3373002f3c0e8c3e819d
Trương Anh Hiếu <91637781+parallelism623@users.noreply.github.com>
2023-12-18 10:31:31 +0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -14,7 +14,7 @@ ARGUMENT:
 def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--cuda-device-no', type=int,
-                    help='cpu: -1, cuda: 0 ~ n', default=0)
+                    help='cpu: -1, cuda: 0 ~ n', default=-1)
     parser.add_argument('--model_load_path', type=str,
                         help='Trained model load path')
     parser.add_argument('--test_content', type=str,
@@ -24,6 +24,11 @@ def build_parser():
     parser.add_argument('--output', type=str,
                         help='output image path to save the stylized image')
     return parser
+
+parser = build_parser()
+args = parser.parse_args()
+os.environ['CUDA_VISIBLE_DEVICES'] = str(args.cuda_device_no)
+network_test(args)
 
 parser = build_parser()
 args = parser.parse_args()
